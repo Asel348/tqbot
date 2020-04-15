64b47f185b912401a817d0ef216e823a9cb95635
Asil <misterman348@hotmail.com>
2020-04-15 16:38:51 +0300

    Update help.js

--- cmd/help.js
+++ cmd/help.js
@@ -3,7 +3,7 @@ module.exports.run = async (client, message, args) => {
     // using try/catch because why not
     try {
 
-        message.channel.send("```--- EĞLENCE --- \n01 - ..catto\n02 - ..doggo\n03 - ..flip\n04 - ..pick [sayı] [arg1] [arg2] ... [argn]\n05 - ..ping\n06 - ..vote [saniye] [başlık]\n07 - ..birb```\n --- MODERASYON --- \n01 - ..ban [mention] [neden]\n02 - ..kick [mention] [neden]\n03 - ..mute [mention]\n04 - ..unmute [mention]")
+        message.channel.send("```--- EĞLENCE --- \n01 - ..catto\n02 - ..doggo\n03 - ..flip\n04 - ..pick [sayı] [arg1] [arg2] ... [argn]\n05 - ..ping\n06 - ..vote [saniye] [başlık]\n07 - ..birb\n --- MODERASYON --- \n01 - ..ban [mention] [neden]\n02 - ..kick [mention] [neden]\n03 - ..mute [mention]\n04 - ..unmute [mention]```")
 
     } catch (err) {
 
