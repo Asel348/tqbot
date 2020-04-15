f4c3e8c3692cea616362ddd7021896fac53b1625
Asil <misterman348@hotmail.com>
2020-04-15 16:38:54 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async client => {
     console.log(`\nHazır!`)
-    console.log(`${client.user.tag} olarak giriş yapıld.!`);
+    console.log(`${client.user.tag} olarak giriş yapıldı!`);
     console.log(`Invite link: ${await client.generateInvite(["ADMINISTRATOR"])}`)
     client.user.setStatus("online");
     client.user.setActivity("..help");
